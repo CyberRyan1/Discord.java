6983745d5b9944536272cbdae9bd62c695bc2a94
CyberRyan <cyberryan934@gmail.com>
2022-02-13 02:01:02 -0600

    Replaced package-info.java with README.md for the internal package

---
+++ src/main/java/com/github/cyberryan1/discordjava/internal/README.md
@@ -0,0 +1,3 @@
+# For Internal Use Only
+This package was intended for internal use. While you are 
+welcome to use this, this is mainly raw discord API handling.
--- src/main/java/com/github/cyberryan1/discordjava/internal/package-info.java
+++
@@ -1,4 +0,0 @@
-package com.github.cyberryan1.discordjava.internal;
-/*
-Nothing in this package should be used by the average user. Most of this is used for library development
- */
