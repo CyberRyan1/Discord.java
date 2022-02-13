89f3a9a947d832c6be583d061a0b70185f58bed2
CyberRyan <cyberryan934@gmail.com>
2022-02-13 00:14:40 -0600

    Reworked the login() a bit and added javadoc comments to it

--- src/main/java/com/github/cyberryan1/discordjava/general/Bot.java
+++ src/main/java/com/github/cyberryan1/discordjava/general/Bot.java
@@ -1,8 +1,11 @@
 package com.github.cyberryan1.discordjava.general;
 
+import com.github.cyberryan1.discordjava.general.errors.ConnectionInterruptedException;
+import com.github.cyberryan1.discordjava.general.errors.InvalidURIException;
+import com.github.cyberryan1.discordjava.general.errors.LoginErrorStatusCodeException;
+import com.github.cyberryan1.discordjava.general.errors.LoginRequestException;
 import com.github.cyberryan1.discordjava.internal.JsonBodyHandler;
 import com.github.cyberryan1.discordjava.internal.http.enums.DiscordLinks;
-import com.github.cyberryan1.discordjava.internal.http.errors.ErrorResponseCodeException;
 import com.github.cyberryan1.discordjava.internal.http.responses.DiscordGateway;
 import com.github.cyberryan1.discordjava.internal.http.websockets.WebsocketManager;
 import com.github.cyberryan1.discordjava.internal.http.websockets.identify.Identify;
@@ -25,22 +28,41 @@ public class Bot {
         this.TOKEN = token;
     }
 
-    // TODO finish javadoc comment for this method
-    public void login() throws IOException, InterruptedException, URISyntaxException {
+    /**
+     * Establishes the connection to the discord API and identifies the bot to the API
+     * @throws LoginRequestException When the request or the response to/from the API cannot be handled properly
+     * @throws LoginErrorStatusCodeException When the response code from the API is an error code
+     * @throws InvalidURIException When the URI link for the websocket to connect to the gateway is invalid
+     * @throws ConnectionInterruptedException When the connection is interrupted by something before connection can be successfully established
+     */
+    public void login() {
         HttpClient httpClient = HttpClient.newHttpClient();
 
-        // requests for the gateway URL that should be used by the bot to connect with
+        // requests for the gateway URL for the websocket that should be used by the bot
         HttpRequest request = HttpRequest.newBuilder( DiscordLinks.GET_GATEWAY.getURI() ).build();
-        HttpResponse<Supplier<DiscordGateway>> response = httpClient.send( request, new JsonBodyHandler<>( DiscordGateway.class ) );
+        HttpResponse<Supplier<DiscordGateway>> response = null;
+        try {
+            response = httpClient.send( request, new JsonBodyHandler<>( DiscordGateway.class ) );
+        } catch ( IOException | InterruptedException e ) {
+            throw new LoginRequestException( e );
+        }
         if ( response.statusCode() >= 400 && response.statusCode() < 500 ) {
-            throw new ErrorResponseCodeException( response.statusCode() );
+            throw new LoginErrorStatusCodeException( "Received error response code "
+                    + response.statusCode() + " when sending a HTTP request" );
         }
         GATEWAY_URL = response.body().get().url;
 
-        // starts the main websocket
-        WebsocketManager.connectMainSocket( GATEWAY_URL );
-        // waits until the websocket is connected
-        WebsocketManager.LATCH.await();
+        try {
+            // starts the main websocket
+            WebsocketManager.connectMainSocket( GATEWAY_URL );
+            // waits until the websocket has been connected
+            WebsocketManager.LATCH.await();
+        } catch ( URISyntaxException e ) {
+            throw new InvalidURIException( GATEWAY_URL, e );
+        } catch ( InterruptedException e ) {
+            throw new ConnectionInterruptedException( e );
+        }
+
         // identifies the bot to the discord API
         new Identify().sendIdentity( TOKEN );
     }
