1450aae090e35942f6a3a7f2dfad577e055f18a4
CyberRyan <cyberryan934@gmail.com>
2022-02-14 17:51:34 -0600

    Created MemberData

---
+++ src/main/java/com/github/cyberryan1/discordjava/internal/classes/MemberData.java
@@ -0,0 +1,17 @@
+package com.github.cyberryan1.discordjava.internal.classes;
+
+import java.util.List;
+
+public class MemberData {
+    public UserData user;
+    public List<String> roles;
+    // TODO add support for "premium_since"
+    public boolean pending;
+    public String nick;
+    public boolean mute;
+    public String joined_at;
+    public String hoisted_role;
+    public boolean deaf;
+    // TODO add support for "communication_disabled_until"
+    // TODO add support for "avatar"
+}
