4bb65e64a6786b38f13ca216b1fb103279187fe1
CyberRyan <cyberryan934@gmail.com>
2022-02-14 17:51:15 -0600

    Created PermissionOverwritesData

---
+++ src/main/java/com/github/cyberryan1/discordjava/internal/classes/PermissionOverwritesData.java
@@ -0,0 +1,10 @@
+package com.github.cyberryan1.discordjava.internal.classes;
+
+public class PermissionOverwritesData {
+    public String type;
+    public String id;
+    public String deny_new;
+    public int deny;
+    public String allow_new;
+    public int allow;
+}
