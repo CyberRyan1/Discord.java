1b2c85efb3290fda5175a6b793cc0d1d5502af6c
CyberRyan <cyberryan934@gmail.com>
2022-02-14 17:51:40 -0600

    Created UserData

---
+++ src/main/java/com/github/cyberryan1/discordjava/internal/classes/UserData.java
@@ -0,0 +1,10 @@
+package com.github.cyberryan1.discordjava.internal.classes;
+
+public class UserData {
+    public String username;
+    public int public_flags;
+    public String id;
+    public String discriminator;
+    public boolean bot;
+    public String avatar;
+}
