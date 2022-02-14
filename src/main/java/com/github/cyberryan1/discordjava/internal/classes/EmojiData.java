4179167d43055654dc244b7419fcfb0ef7446050
CyberRyan <cyberryan934@gmail.com>
2022-02-14 17:50:06 -0600

    Created the EmojiData class

---
+++ src/main/java/com/github/cyberryan1/discordjava/internal/classes/EmojiData.java
@@ -0,0 +1,11 @@
+package com.github.cyberryan1.discordjava.internal.classes;
+
+public class EmojiData {
+    // TODO add support for the "roles" part of this json
+    public boolean require_colons;
+    public String name;
+    public boolean managed;
+    public String id;
+    public boolean available;
+    public boolean animated;
+}
